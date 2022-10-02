fa12a6a9315554235f5b12d752404fa21b3a910e
wzxcs <114869350+wzxcs@users.noreply.github.com>
2022-10-02 20:59:12 +0530

    added useState

---
+++ React Hooks/useState.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function FavoriteColor() {
+ const [color, setColor] = useState("red");
+    
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor("blue")}
+      >Blue</button>
+      <button
+        type="button"
+        onClick={() => setColor("red")}
+      >Red</button>
+      <button
+        type="button"
+        onClick={() => setColor("pink")}
+      >Pink</button>
+      <button
+        type="button"
+        onClick={() => setColor("green")}
+      >Green</button>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FavoriteColor />);
+
+              
